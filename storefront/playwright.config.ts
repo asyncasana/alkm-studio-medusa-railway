9b6c50654a613f7b9a2ef6584acffdd2f74c8218
asyncasana <dash.boyeva@gmail.com>
2025-08-01 14:08:28 +0100

    chore: switch to npm-only workflow, remove pnpm/yarn references and update scripts

--- storefront/playwright.config.ts
+++ storefront/playwright.config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-     command: 'yarn start',
+     command: 'npm run dev',
      url: process.env.NEXT_PUBLIC_BASE_URL,
   //   reuseExistingServer: !process.env.CI,
   },
